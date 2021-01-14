d8fcf8e98f9fff4d2c9e6fd7f5abd120c03d0520
Yuxuan Xie <teststaybaka@gmail.com>
2021-01-14 21:52:11 +0000

    Initial migration of cleaner.

---
+++ build/cleaner.ts
@@ -0,0 +1,43 @@
+import fs = require("fs");
+import path = require("path");
+
+export async function clean(rootDir: string): Promise<void> {
+  let files = await findFiles(rootDir);
+  let promisesToUnlink = files.map(async (file) => {
+    await fs.promises.unlink(file);
+  });
+  await Promise.all(promisesToUnlink);
+}
+
+export async function findFiles(rootDir: string): Promise<Array<string>> {
+  let files = await findFilesRecursively(rootDir);
+  return files.filter(
+    (file) =>
+      file.endsWith(".d.ts") ||
+      file.endsWith(".js") ||
+      file.endsWith(".tsbuildinfo")
+  );
+}
+
+async function findFilesRecursively(dir: string): Promise<Array<string>> {
+  let items = await fs.promises.readdir(dir);
+  let files: string[] = [];
+  let promisesOfFiles = items.map(
+    async (item): Promise<void> => {
+      let fullPath = path.join(dir, item);
+      let fileStats = await fs.promises.stat(fullPath);
+      if (fileStats.isDirectory()) {
+        if (item === "node_modules") {
+          return;
+        } else {
+          let filesFromSubDirectory = await findFilesRecursively(fullPath);
+          files.push(...filesFromSubDirectory);
+        }
+      } else {
+        files.push(fullPath);
+      }
+    }
+  );
+  await Promise.all(promisesOfFiles);
+  return files;
+}
---
+++ build/cleaner_test.ts
@@ -0,0 +1,29 @@
+import { findFiles } from "./cleaner";
+import { assertThat, eq, eqArray } from "@selfage/test_base/matcher";
+import { TEST_RUNNER } from "@selfage/test_base/runner";
+
+TEST_RUNNER.run({
+  name: "CleanerTest",
+  cases: [
+    {
+      name: "FindFilesIncludingNested",
+      execute: async () => {
+        // Execute
+        let files = await findFiles("./test_data/build/cleaner");
+
+        // Verify
+        assertThat(
+          files,
+          eqArray([
+            eq("test_data/build/cleaner/random.d.ts"),
+            eq("test_data/build/cleaner/random.js"),
+            eq("test_data/build/cleaner/ts.tsbuildinfo"),
+            eq("test_data/build/cleaner/inside/random.d.ts"),
+            eq("test_data/build/cleaner/inside/random.js"),
+          ]),
+          "files"
+        );
+      },
+    },
+  ],
+});
