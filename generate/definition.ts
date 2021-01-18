7e4116eb6d07597736458f07ba871ad6ab940eec
Yuxuan Xie <teststaybaka@gmail.com>
2021-01-18 05:52:09 +0000

    Add comment for message definition

--- generate/definition.ts
+++ generate/definition.ts
@@ -12,14 +12,17 @@ export interface EnumDefinition {
 
 export interface MessageFieldDefinition {
   name: string;
+  // Can be 'number', 'string', 'boolean' or the name of a message or enum.
   type: string;
   isArray?: true;
+  // Resolves import path the same way as Node. Do not include '.json'.
   import?: string;
   comment?: string;
 }
 
 export interface MessageExtendDefinition {
   name: string;
+  // Resolves import path the same way as Node. Do not include '.json'.
   import?: string;
 }
 
